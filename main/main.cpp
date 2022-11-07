3f2f12b1d1358921c5af410f848e2e807f7599d7
Alexey <73526308+AurionEA@users.noreply.github.com>
2022-11-07 20:36:13 +0300

    Последние изменения в лабораторной
    Включая все изменения за последние две недели и все сегодняшние изменения в коде

--- main/main.cpp
+++ main/main.cpp
@@ -2,12 +2,18 @@
 
 #include <stdio.h>
 #include "../lib_example/example.h"
+#include "matrix.h"
 
-void main() {
+int main() {
   int a = 1, b = 4;
   float result;
+
+  TDynamicVector<int> vect(10);
+  vect[3] = 7;
 
   result = division(a, b);
 
   printf("%d / %d = %.2f\n", a, b, result);
+  std::cout << vect[3] << std::endl << vect;
+  return 0;
 }
